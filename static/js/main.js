11748bd058bbb611f829c66bfe9af6a226f18e29
genesis.leon <genesis.leon@glr.pe>
2017-11-22 17:42:26 -0500

    avance chart

--- static/js/main.js
+++ static/js/main.js
@@ -30,47 +30,151 @@ particlesJS.load('particles-js', './static/js/particles.json', function() {
       $(this).addClass("active");
     })
 
-    function graphic(){
-      var height = 500;
+    /*grafico*/
+function graphic(){
+    var margin = {top: 20, right: 20, bottom: 70, left: 40},
+    width = 600 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-      var svg = d3.select("#graphic").append("svg")
-      .attr("width", altura)
-      .attr("height", height);
-      d3.csv("../static/documentos/encuesta.csv", function(d){
+    var svg = d3.select("#graphic").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
 
-        var r = d3.scale.linear()
-        .domain([0, d3.max(d, function(d) { return parseFloat(d.Porcentaje); })])
-        .range([40, 300]);
+    d3.csv("../static/documentos/encuesta.csv", function(error, data) {
 
-        svg.selectAll("rect")
-        .data(d)
-        .enter()
-        .append("rect")
-        .attr("x", function(d, i) {return i*70;})
-        .attr("y", 500)
-        .attr("width", 60)
-        .attr("height", 0)
-        .attr("fill", function(d){ return d3.rgb(d.Color).darker();})
-        .transition()
-        .duration(1000)
-        .delay(function(d, i){return i * 200;})
-        .ease("linear")
-        .attr("y", function(d) { return 500 - r(d.Porcentaje); })
-        .attr("height", function(d) { return r(d.Porcentaje); })
-        .attr("fill", function(d){ return d3.rgb(d.Color); });
+        data.forEach(function(d) {
+            d.Partido = d.Partido;
+            d.Porcentaje = +d.Porcentaje;
+        });
 
-        svg.selectAll(".partido")
-        .data(d)
-        .enter()
-        .append("text")
-        .attr("class","partido")
-        .text(function(d){return d.Partido;})
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("transform", function(d, i) {return "translate("+((i*50)+26)+",95) rotate(-90)";});
-      });
-    }
-    // graphic()
+      var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+      var y = d3.scale.linear().domain([0, d3.max(data, function(d) { return parseFloat(d.Porcentaje); })]).range([20, height]);
+
+      x.domain(data.map(function(d) { return d.Partido; }));
+      y.domain([0, d3.max(data, function(d) { return d.Porcentaje; })]);
+
+      var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom")
+
+      var tip = d3.tip()
+          .attr('class', 'd3-tip')
+          .offset([-10, 0])
+          .html(function(d) {
+            return "<strong>Frequency:</strong> <span style='color:red'>" + d.Texto + ".</span>";
+          })
+          svg.call(tip);
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(10," + height + ")")
+          .call(xAxis)
+        .selectAll("text")
+          .style("text-anchor", "end")
+          .attr("dx", "2em")
+          .attr("dy", "-.55em")
+          .attr("transform", "translate(15, 20)" );
+
+      // svg.append("g")
+      //     .attr("class", "y axis")
+      //     .call(yAxis)
+      //   .append("text")
+      //     .attr("transform", "rotate(-90)")
+      //     .attr("y", 6)
+      //     .attr("dy", ".71em")
+      //     .style("text-anchor", "end")
+      //     .text("Value ($)");
+
+      svg.selectAll("bar")
+          .data(data)
+        .enter().append("rect")
+          .style("fill", "steelblue")
+          .attr("x", function(d) { return x(d.Partido) + 10; })
+          .attr("width", x.rangeBand())
+          .attr("y", height)
+          .attr("height", 0)
+          .on('mouseover', tip.show)
+          .on('mouseout', tip.hide)
+          .transition()
+          .duration(1000)
+          .delay(function(d, i){return i * 200;})
+          .ease("linear")
+          .attr("y", function(d) { return height - y(d.Porcentaje); })
+          .attr("height", function(d) { return y(d.Porcentaje); })
+          // .attr("fill", function(d){ return d3.rgb(d.Color); })
+    });
+}
+
+    // function graphic(){
+    //
+    //   var margin = {top: 20, right: 20, bottom: 70, left: 40};
+    //   var width = altura/2;
+    //   var height = 510;
+    //
+    //   var svg = d3.select("#graphic").append("svg")
+    //   .attr("width", width)
+    //   .attr("height", height);
+    //
+    //   // var	parseDate = d3.time.format("%Y-%m").parse;
+    //   var x = d3.scale.ordinal().rangeRoundBands([0, height], .5, .5);
+    //   var xAxis = d3.svg.axis()
+    //     .scale(x)
+    //     .ticks(8)
+    //     .orient("bottom");
+    //
+    //   d3.csv("../static/documentos/encuesta.csv", function(d){
+    //
+    //     x.domain(d.map(function(d) { return d.Partido; }));
+    //
+    // svg.append("g")
+    //   .attr("class", "x axis")
+    //   .attr("transform", "translate(10,490)")
+    //   .call(xAxis)
+    // .selectAll("text")
+    //   .style("text-anchor", "middle")
+    //   .attr("dx", "10px")
+    //   .attr("dy", "4")
+    //   .attr("transform", "translate(-30,0)");
+    //
+    //     var tip = d3.tip()
+    //     .attr('class', 'd3-tip')
+    //     .offset([-10, 0])
+    //     .html(function(d) {
+    //       return "<strong>Frequency:</strong> <span style='color:red'>" + d.Texto + ".</span>";
+    //     })
+    //     svg.call(tip);
+    //
+    //     var r = d3.scale.linear()
+    //     .domain([0, d3.max(d, function(d) { return parseFloat(d.Porcentaje); })])
+    //     .range([10, 500]);
+    //
+    //     svg.selectAll("rect")
+    //     .data(d)
+    //     .enter()
+    //     .append("rect")
+    //     .attr("x", function(d, i) {return i*80 +10;})
+    //     .attr("y", 490)
+    //     .attr("width", 70)
+    //     .attr("height", 0)
+    //     .on('mouseover', tip.show)
+    //     .on('mouseout', tip.hide)
+    //     .attr("fill", function(d){ return d3.rgb(d.Color).darker();})
+    //     .transition()
+    //     .duration(1000)
+    //     .delay(function(d, i){return i * 200;})
+    //     .ease("linear")
+    //     .attr("y", function(d) { return 490 - r(d.Porcentaje); })
+    //     .attr("height", function(d) { return r(d.Porcentaje); })
+    //     .attr("fill", function(d){ return d3.rgb(d.Color); })
+    //
+    //   });
+    //
+    // }
+
+
     var controller = new ScrollMagic.Controller(),
         scene_1,
         scene_2;
@@ -79,6 +183,7 @@ particlesJS.load('particles-js', './static/js/particles.json', function() {
     triggerElement: '#about',
     triggerHook: 0.7
   })
+  .addIndicators()
   .addTo(controller)
 
   scene_1.on("progress", function(event) {
@@ -89,16 +194,15 @@ particlesJS.load('particles-js', './static/js/particles.json', function() {
 
   scene_2 = new ScrollMagic.Scene({
                 triggerElement: '#graphic-container',
-                triggerHook: 0.7,
+                duration: 300,
+                triggerHook: 0.5,
                 reverse:false
               })
+              .setPin("#graphic-container")
               .addIndicators()
               .addTo(controller)
 
-  scene_2.on("start", function(event) {
-    console.log("leave")
-      graphic()
-    })
+  scene_2.on("enter", graphic)
     .addTo(controller);
 
 
